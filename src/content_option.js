fd1ec07b2024a34fd13499db247ccf62b374d548
Ricma <ric.madse@gmail.com>
2022-09-17 21:06:52 -0600

    template added

---
+++ src/content_option.js
@@ -0,0 +1,168 @@
+const logotext = "Ric.Madse";
+const meta = {
+    title: "Eric Madsen",
+    description: "I'm Eric Madsen, a web development student",
+};
+
+const introdata = {
+    title: "Eric Madsen",
+    animated: {
+        first: "React",
+        second: "MongoDB",
+        third: "Node",
+    },
+    description: "Full-stack development student at The University of Utah.",
+    your_img_url: "https://scontent.fslc3-2.fna.fbcdn.net/v/t39.30808-6/307118224_1137573950175237_5241604668967108881_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=v9frwY4IDxMAX98Ugyz&_nc_ht=scontent.fslc3-2.fna&oh=00_AT_lHWCdHlk8gJYhUgIMjblbCFHht8ekW-RNso4GBP68-A&oe=632BA591",
+};
+
+const dataabout = {
+    title: "Who I Am",
+    aboutme: "I'm an area manager with The Industrial Fumigant Company and a full-stack development student at The University of Utah.",
+};
+const worktimeline = [{
+        jobtitle: "Web Development Boot Camp",
+        where: "U of U",
+        date: "April 2022 to Current",
+    },
+    {
+        jobtitle: "Package Handler",
+        where: "FedEx",
+        date: "June 2021 to Current",
+    },
+    {
+        jobtitle: "Area Manager",
+        where: "The Industrial Fumigant Co.",
+        date: "November 2009 to Current",
+    },
+];
+
+const skills = [{
+        name: "Python",
+        value: 90,
+    },
+    {
+        name: "Djano",
+        value: 85,
+    },
+    {
+        name: "Javascript",
+        value: 80,
+    },
+    {
+        name: "React",
+        value: 60,
+    },
+    {
+        name: "Jquery",
+        value: 85,
+    },
+];
+
+const services = [{
+        title: "UI & UX Design",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+    },
+    {
+        title: "Mobile Apps",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+    },
+    {
+        title: "Wordpress Design",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+    },
+];
+
+const dataportfolio = [{
+        img: "https://picsum.photos/400/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/800/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/550/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "ric.madse@gmail.com",
+    YOUR_FONE: "(801)662-8509",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vehicula eu nunc et sollicitudin. Cras pulvinar, nisi at imperdiet pharetra. ",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_id",
+    YOUR_TEMPLATE_ID: "template_id",
+    YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    github: "https://github.com/LimeRicky84",
+    facebook: "https://facebook.com",
+    linkedin: "https://www.linkedin.com/in/eric-madsen-302145236/",
+    twitter: "https://twitter.com",
+    youtube: "https://youtube.com",
+    twitch: "https://twitch.com",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
